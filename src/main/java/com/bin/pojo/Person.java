617153aaf5bd25eb4b83acd47286c0be3b66cc47
Lostnt <674182722@qq.com>
2019-07-17 22:41:20 +0800

    1.0

--- src/main/java/com/bin/pojo/Person.java
+++ src/main/java/com/bin/pojo/Person.java
@@ -2,7 +2,21 @@ package com.bin.pojo;
 
 public class Person {
     String uname;
-    String upwd;
+    String pwd;
 
+    public String getUname() {
+        return uname;
+    }
 
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
 }
