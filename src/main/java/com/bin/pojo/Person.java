894347e3e04538a1c1f7b6d3cccb767789689201
Azrael006 <2030858003@qq.com>
2019-07-17 22:42:58 +0800

    Person

--- src/main/java/com/bin/pojo/Person.java
+++ src/main/java/com/bin/pojo/Person.java
@@ -4,5 +4,19 @@ public class Person {
     String uname;
     String upwd;
 
+    public String getUname() {
+        return uname;
+    }
 
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getUpwd() {
+        return upwd;
+    }
+
+    public void setUpwd(String upwd) {
+        this.upwd = upwd;
+    }
 }
