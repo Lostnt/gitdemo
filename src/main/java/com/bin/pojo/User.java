394798981231248d6bebbcfa9d9a1f71ec14a892
Lostnt <674182722@qq.com>
2019-07-17 22:52:15 +0800

    1.0

--- src/main/java/com/bin/pojo/User.java
+++ src/main/java/com/bin/pojo/User.java
@@ -1,5 +1,22 @@
 package com.bin.pojo;
 
 public class User {
-    String username;
+    String a;
+    String b;
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
 }
