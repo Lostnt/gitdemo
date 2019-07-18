96f1defa1fd7ed28e3d5db40cd11eeac76276a13
Lostnt <674182722@qq.com>
2019-07-18 10:16:56 +0800

    1.1

---
+++ src/main/java/com/bin/pojo/Animal.java
@@ -0,0 +1,22 @@
+package com.bin.pojo;
+
+public class Animal {
+    String play;
+    String age;
+
+    public String getPlay() {
+        return play;
+    }
+
+    public void setPlay(String play) {
+        this.play = play;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
